4895e7c87995ce6e9e9a21c3cab6002e3b99f4b9
jordanabe23 <jabrahamny23@gmail.com>
2024-09-28 22:41:59 -0400

    Refactor registration endpoint to login endpoint

---
+++ src/app/register/page.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const RedirectPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
+
+  return null;
+};
+
+export default RedirectPage;
